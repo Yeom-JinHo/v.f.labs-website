14f5f1ca104f80b1ef0121c0f84b77ba54850304
Yeom-JinHo <pwlsghq@naver.com>
2025-07-19 22:11:41 +0900

    feat: source 쪽에 임시로 artist 세팅

--- apps/web/src/app/source.ts
+++ apps/web/src/app/source.ts
@@ -1,3 +1,4 @@
+import type { ArtistProfile } from "@/types/artist";
 import { createMDXSource } from "@fumadocs/content-collections";
 import {
   allBlogMetas,
@@ -16,3 +17,36 @@ export const blog = loader({
   baseUrl: "/blog",
   source: createMDXSource(allBlogs, allBlogMetas),
 });
+
+export const artistProfilesData: ArtistProfile[] = [
+  {
+    name: "Ho",
+    image: "/images/person/ho.jpeg",
+    nickname: "seungho",
+    description: "그는 감히 전설이라고 할 수 있다.",
+  },
+  {
+    name: "JinHo",
+    image: "/images/person/jinho.png",
+    nickname: "yeom2",
+    description: "엄마 나는 커서 승호처럼 될래요.",
+  },
+  {
+    name: "Kingtaro",
+    image: "/images/person/kingtaro.png",
+    nickname: "juntaro",
+    description: "King",
+  },
+  {
+    name: "Mr. Dong",
+    image: "/images/person/dong.jpg",
+    nickname: "dminzz",
+    description: "Dong",
+  },
+];
+
+export const artistProfile = {
+  baseUrl: "/artistProfiles",
+  getPages: () => artistProfilesData,
+  getPage: (name: string) => artistProfilesData.find((p) => p.name === name),
+};
