1e2fe7b3afff35fd794f8ec6e7d4eba50626d415
Yeom-JinHo <pwlsghq@naver.com>
2025-07-20 11:54:15 +0900

    feat: 아티스트 에셋 정리

---
+++ apps/web/src/consts/artist.ts
@@ -0,0 +1,9 @@
+export const ARTIST_NAME = {
+  JUNTARO: "Juntaro",
+  SIELO: "Sielo",
+  SAM: "SAM",
+  DEARBOI: "DearBoi",
+  PLAYMODE: "PLAYMODE",
+  LOOZBONE: "Loozbone",
+  RYUMO: "RYUMO",
+} as const;
--- apps/web/src/types/artist.ts
+++ apps/web/src/types/artist.ts
@@ -1,10 +1,14 @@
+import type { ARTIST_NAME } from "@/consts/artist";
+
 import type { Socials } from "./contact";
 
 export interface ArtistProfile {
-  name: string;
+  name: ARTIST_NAME;
   image: string;
-  nickname: string;
+  nickname: ARTIST_NAME;
   shortDescription: string;
   fullDescription: string;
   socials?: Socials[];
 }
+
+export type ArtistName = (typeof ARTIST_NAME)[keyof typeof ARTIST_NAME];
