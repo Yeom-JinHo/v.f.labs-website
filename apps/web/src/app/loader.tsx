e01f54a4c8c176889b78d242f7196287665aabb8
Yeom-JinHo <pwlsghq@naver.com>
2025-07-20 14:54:03 +0900

    feat: loader 커스텀

--- apps/web/src/app/loader.tsx
+++ apps/web/src/app/loader.tsx
@@ -1,25 +1,50 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NumberTicker from "@/components/fancy/number-ticker";
+import TextReveal from "@/components/fancy/text-reveal";
 import { Preloader } from "@/components/preloader";
 import { AnimatePresence } from "motion/react";
 
 export default function Loader() {
   const [isVisible, setIsVisible] = useState(true);
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (isReady) {
+      const timer = setTimeout(() => {
+        setIsVisible(false);
+      }, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [isReady]);
 
   return (
     <AnimatePresence mode="wait">
       {isVisible && (
         <Preloader>
-          <NumberTicker
-            from={10}
-            target={100}
-            autoStart={true}
-            transition={{ duration: 2.5, type: "tween", ease: "easeInOut" }}
-            onComplete={() => setIsVisible(false)}
-            onStart={() => console.log("start")}
-          />
+          {!isReady ? (
+            <NumberTicker
+              from={10}
+              target={100}
+              autoStart={true}
+              transition={{ duration: 2, type: "tween", ease: "easeInOut" }}
+              onComplete={() => setIsReady(true)}
+              onStart={() => console.log("start")}
+            />
+          ) : (
+            <>
+              <TextReveal className="text-6xl font-bold xl:text-[12rem]">
+                Are
+              </TextReveal>
+              <TextReveal className="ml-2 text-6xl font-bold xl:ml-10 xl:text-[12rem]">
+                you
+              </TextReveal>
+              <TextReveal className="ml-2 text-6xl font-bold xl:ml-10 xl:text-[12rem]">
+                ready?
+              </TextReveal>
+            </>
+          )}
         </Preloader>
       )}
     </AnimatePresence>
