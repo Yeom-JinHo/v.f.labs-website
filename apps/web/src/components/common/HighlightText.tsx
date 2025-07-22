9ad9f4c2f1934e4cd2dc7e0d7e77984267b80534
Yeom-JinHo <pwlsghq@naver.com>
2025-07-22 20:59:40 +0900

    feat: isHover 상위에서 관리하도록 변경

--- apps/web/src/components/common/HighlightText.tsx
+++ apps/web/src/components/common/HighlightText.tsx
@@ -1,29 +1,26 @@
 import type { ReactNode } from "react";
-import { useState } from "react";
 import { motion } from "motion/react";
 
 interface HighlightTextProps {
   children: ReactNode;
   onHover: (text: string) => void;
+  isHover?: boolean;
   imageUrl?: string;
   isMobile?: boolean;
 }
 
 function HighlightText({
   children,
+  isHover,
   onHover,
   imageUrl,
   isMobile,
 }: HighlightTextProps) {
-  const [hovered, setHovered] = useState(false);
-
   const handleMouseEnter = () => {
-    setHovered(true);
     onHover(children as string);
   };
 
   const handleMouseLeave = () => {
-    setHovered(false);
     onHover("");
   };
 
@@ -45,12 +42,12 @@ function HighlightText({
           className="absolute inset-0 z-0"
           style={{
             backgroundImage: `url(${imageUrl})`,
-            backgroundSize: "cover",
+            backgroundSize: "contain",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
           }}
           initial={{ opacity: 0 }}
-          animate={{ opacity: hovered ? 0.6 : 0 }}
+          animate={{ opacity: isHover ? 0.6 : 0 }}
           transition={{ duration: 0.5 }}
         />
       )}
@@ -66,14 +63,14 @@ function HighlightText({
         <motion.span
           className="align-center pointer-events-none absolute inset-0 whitespace-nowrap"
           initial={{ width: 0 }}
-          animate={{ width: hovered ? "100%" : "0%" }}
+          animate={{ width: isHover ? "100%" : "0%" }}
           transition={{ duration: 0.5 }}
           style={{
             color: "white",
             WebkitTextStroke: "1px white",
-            borderRight: hovered ? "6px solid white" : "0px solid transparent",
+            borderRight: isHover ? "6px solid white" : "0px solid transparent",
             overflow: "hidden",
-            filter: hovered ? "drop-shadow(0 0 3px white)" : "none",
+            filter: isHover ? "drop-shadow(0 0 3px white)" : "none",
           }}
         >
           {children}
