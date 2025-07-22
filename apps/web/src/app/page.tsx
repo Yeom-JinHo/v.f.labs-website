57e99a621da9a00da16b9e85038fc167db54cd62
Yeom-JinHo <pwlsghq@naver.com>
2025-07-22 21:54:13 +0900

    feat: home 페이지 clear

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,18 +5,9 @@ export default function Home() {
   return (
     <>
       <div className="flex min-h-[100dvh] flex-col">
-        {/* <Header /> */}
         <main className="flex-1">
           <Main />
-          {/* <Hero /> */}
-          {/* <About /> */}
-          {/* <Skills />
-          <Projects />
-          <Experience />
-          <Testimonials /> */}
-          {/* <ArtistProfiles /> */}
         </main>
-        <footer>{/* <Footer /> */}</footer>
       </div>
       <Cursor />
     </>
