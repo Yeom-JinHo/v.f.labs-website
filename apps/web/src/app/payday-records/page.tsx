99c780e804938652e352dcdfac5d39196a537d86
Yeom-JinHo <pwlsghq@naver.com>
2025-07-22 23:36:19 +0900

    fix: 올바르지 않는 css 변경

--- apps/web/src/app/payday-records/page.tsx
+++ apps/web/src/app/payday-records/page.tsx
@@ -2,7 +2,7 @@ export default function PaydayRecordsPage() {
   return (
     <div className="flex min-h-[100dvh] flex-col">
       <main className="flex-1">
-        <h1 className="font-6xl">TBD</h1>
+        <h1 className="text-6xl">TBD</h1>
       </main>
     </div>
   );
