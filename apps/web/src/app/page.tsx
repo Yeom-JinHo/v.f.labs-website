6a35b3f0e917b42b1ce27e8a34964ca874ee0326
Yeom-JinHo <81306489+Yeom-JinHo@users.noreply.github.com>
2025-07-22 21:25:25 +0900

    feat: Main  페이지 재개발 (#15)
    * wip
    
    * feat: main 페이지 수정 1차
    
    * feat: main 페이지 헤더 제거
    
    * feat: HighlightText 컴포넌트 구현
    
    * feat: highlighter White color로 수정
    
    * feat: 뒤에 로고까지
    
    * feat: 하이라이트텍스트 모바일 대응
    
    * feat: isHover 상위에서 관리하도록 변경
    
    * feat: logo asset 추가
    
    * feat: 메인화면 완성
    
    * Revert "wip"
    
    This reverts commit d68be744bec38e4e3cd21ffc7afc21948134ae5b.
    
    * refactor: type 명확하게
    
    * refactor: 높이 정확하게 명시
    
    * refactor: company name 상수화
    
    * refactor: 에셋 이름들 상수화

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,29 +1,22 @@
 import Cursor from "@/components/cursor/cursor";
-import {
-  About,
-  ArtistProfiles,
-  Footer,
-  Header,
-  Hero,
-} from "@/components/sections";
+import Main from "@/components/sections/main/modern/main";
 
 export default function Home() {
   return (
     <>
       <div className="flex min-h-[100dvh] flex-col">
-        <Header />
+        {/* <Header /> */}
         <main className="flex-1">
-          <Hero />
-          <About />
+          <Main />
+          {/* <Hero /> */}
+          {/* <About /> */}
           {/* <Skills />
           <Projects />
           <Experience />
           <Testimonials /> */}
-          <ArtistProfiles />
+          {/* <ArtistProfiles /> */}
         </main>
-        <footer>
-          <Footer />
-        </footer>
+        <footer>{/* <Footer /> */}</footer>
       </div>
       <Cursor />
     </>
