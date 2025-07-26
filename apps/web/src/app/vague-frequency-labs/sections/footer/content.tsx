b3078f0ed64e2a337be91123d6958eff7ee80a79
Yeom-JinHo <81306489+Yeom-JinHo@users.noreply.github.com>
2025-07-26 11:41:03 +0900

    feat: v-f-labs hero 이미지 구현 & 컴포넌트들 v-f-labs 하위로 이동 (#21)
    * feat: v-f-labs에 section 폴더 옮기기
    
    * feat: Hero 부분 1차 구현
    
    * fix: window 객체 제거
    
    * feat: 에셋 추가
    
    * feat: 모바일 대응
    
    * feat: hero 문구 추가
    
    * feat: 문구 디테일 수정 height 수정
    
    * feat: carousel 하나 제거
    
    * fix: x축 스크롤 안생기게
    
    * refactor: HeroImage 컴포넌트 분리
    
    * feat: hero 에셋 추가
    
    * refactor:import path 정리
    
    * refactor: useTransForm
    
    * fix: ArtistProfile import
    
    * fix: import path

---
+++ apps/web/src/app/vague-frequency-labs/sections/footer/content.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { metadata as meta } from "@/app/config";
+import Link from "@/components/fancy/link";
+import { contact } from "@/components/sections/contact/config";
+
+export default function Content() {
+  return (
+    <div className="bg-muted/30 flex h-full w-full flex-col justify-between px-12 py-8">
+      <Nav />
+      <Copyright />
+    </div>
+  );
+}
+
+const Copyright = () => {
+  return (
+    <div className="flex flex-col items-start justify-between sm:flex-row sm:items-end">
+      <h1 className="mt-10 text-[18vw] leading-[0.8] md:text-[16vw] lg:text-[18vw] xl:text-[20vw] 2xl:text-[22vw]">
+        v.f.labs
+      </h1>
+      <p className="mt-4 text-xs sm:mt-0 sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
+        © {2025} {meta.author.name}
+      </p>
+    </div>
+  );
+};
+
+const Nav = () => {
+  return (
+    <div className="flex shrink-0 gap-20">
+      {/* <div className="flex flex-col gap-2">
+        <h3 className="mb-2 text-zinc-500 uppercase dark:text-zinc-400">
+          About
+        </h3>
+        {links.map((link, index) => {
+          const { title, href } = link;
+
+          return (
+            <Link
+              className="underline-offset-4 hover:underline"
+              href={href}
+              key={`ft-l_about_${index}`}
+            >
+              {title}
+            </Link>
+          );
+        })}
+      </div> */}
+      <div className="flex flex-col gap-2">
+        <h3 className="mb-2 text-zinc-500 uppercase dark:text-zinc-400">
+          Socials
+        </h3>
+        {contact.socials.map((link, index) => {
+          const { name, href, Icon } = link;
+
+          return (
+            <Link
+              className="underline-offset-4 hover:underline"
+              href={href}
+              target="_blank"
+              key={`ft-l_social_${index}`}
+              external
+            >
+              {Icon && <Icon className="h-4 w-4" />}
+              {name}
+            </Link>
+          );
+        })}
+      </div>
+      {/* <div className="flex flex-col gap-2">
+        <h3 className="mb-2 text-zinc-500 uppercase dark:text-zinc-400">
+          More
+        </h3>
+        {footer.map((link, index) => {
+          const { title, href } = link;
+
+          return (
+            <Link
+              className="underline-offset-4 hover:underline"
+              href={href}
+              key={`ft-l_more_${index}`}
+            >
+              {title}
+            </Link>
+          );
+        })}
+      </div> */}
+    </div>
+  );
+};
--- apps/web/src/components/sections/artistProfiles/config.ts
+++
@@ -1,4 +0,0 @@
-import type { ArtistProfile } from "@/types/artist";
-import { artistProfile as artistProfileData } from "@/app/source";
-
-export const artistProfiles: ArtistProfile[] = artistProfileData.getPages();
