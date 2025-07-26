14ebd0c1ded503d5bf3c97f63110fe1793810cb0
Yeom-JinHo <pwlsghq@naver.com>
2025-07-26 13:46:17 +0900

    feat: MusicInfoCard에 desc들 추가

--- apps/web/src/app/vague-frequency-labs/sections/musicList/MusicInfoCard.tsx
+++ apps/web/src/app/vague-frequency-labs/sections/musicList/MusicInfoCard.tsx
@@ -13,45 +13,53 @@ function MusicInfoCard({ musicInfo }: MusicInfoProps) {
   const [isAnimating, setIsAnimating] = useState(false);
 
   return (
-    <motion.div
-      onHoverStart={() => setIsAnimating(true)}
-      onHoverEnd={() => setIsAnimating(false)}
-      animate={
-        isAnimating
-          ? {
-              rotate: [0, 360],
-              borderRadius: "50%",
-            }
-          : {
-              rotate: 0,
-              borderRadius: "0%",
-            }
-      }
-      transition={
-        isAnimating
-          ? {
-              rotate: {
-                duration: 3,
-                repeat: Infinity,
-                ease: "linear",
-              },
-              borderRadius: { duration: 0.4 },
-            }
-          : {
-              duration: 0.4,
-            }
-      }
-      onTap={() => setIsAnimating(true)}
-      className="overflow-hidden"
-    >
-      <Image
-        src={musicInfo.image}
-        alt={musicInfo.name}
-        width={300}
-        height={300}
-        className="h-full w-full object-cover"
-      ></Image>
-    </motion.div>
+    <div className="width-[360px] h-[360px]">
+      <motion.div
+        onHoverStart={() => setIsAnimating(true)}
+        onHoverEnd={() => setIsAnimating(false)}
+        animate={
+          isAnimating
+            ? {
+                rotate: [0, 360],
+                borderRadius: "50%",
+              }
+            : {
+                rotate: 0,
+                borderRadius: "0%",
+              }
+        }
+        transition={
+          isAnimating
+            ? {
+                rotate: {
+                  duration: 3,
+                  repeat: Infinity,
+                  ease: "linear",
+                },
+                borderRadius: { duration: 0.4 },
+              }
+            : {
+                duration: 0.4,
+              }
+        }
+        onTap={() => setIsAnimating(true)}
+        className="overflow-hidden"
+      >
+        <Image
+          src={musicInfo.image}
+          alt={musicInfo.name}
+          width={300}
+          height={300}
+          className="h-full w-full object-cover"
+        ></Image>
+      </motion.div>
+      {isAnimating && (
+        <>
+          <div className="text-3xl">{musicInfo.name}</div>
+          <div className="text-xl">{musicInfo.artist}</div>
+        </>
+      )}
+    </div>
   );
 }
 
