24a69f806a6a4b5c7df5c83fda2f9cdd8a19ed28
Yeom-JinHo <pwlsghq@naver.com>
2025-07-29 20:00:34 +0900

    feat: 모바일 사이즈 수정

--- apps/web/src/app/vague-frequency-labs/sections/artistProfiles/ArtistSimpleCard.tsx
+++ apps/web/src/app/vague-frequency-labs/sections/artistProfiles/ArtistSimpleCard.tsx
@@ -12,7 +12,7 @@ interface ArtistSimpleCardProps {
 export default function ArtistSimpleCard({ artist }: ArtistSimpleCardProps) {
   return (
     <div>
-      <div className="group relative h-[300px] w-[200px] overflow-hidden object-cover md:h-[520px] md:w-[350px]">
+      <div className="group relative h-[400px] w-[300px] overflow-hidden object-cover md:h-[520px] md:w-[350px]">
         {/* 기본 이미지 */}
         {artist.name === ARTIST_NAME.LOOZBONE ||
         artist.name === ARTIST_NAME.SAM ? (
