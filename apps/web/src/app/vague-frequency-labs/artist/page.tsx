58fff271ac38c6c5476db3ca6c87e4a316e62e2f
Yeom-JinHo <pwlsghq@naver.com>
2025-07-29 19:21:56 +0900

    feat: 아티스트 페이지 1차 구현

--- apps/web/src/app/vague-frequency-labs/artist/page.tsx
+++ apps/web/src/app/vague-frequency-labs/artist/page.tsx
@@ -1,9 +1,12 @@
 import type { CollectionPage, WithContext } from "schema-dts";
 import React from "react";
+import Image from "next/image";
 import { metadata as meta } from "@/app/config";
-import { project } from "@/app/source";
+import { artistProfile, project } from "@/app/source";
+import { CloudinaryImage } from "@/components/cloudinary-image";
 import Line from "@/components/fancy/line";
 import TextReveal from "@/components/fancy/text-reveal";
+import { ARTIST_NAME } from "@/consts/artist";
 import { createMetadata } from "@/lib/metadata";
 
 const title = "Artist";
@@ -60,10 +63,42 @@ export default function ProjectsPage(): React.ReactElement {
             as="h1"
             className="leading-wide tracking-relaxed text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl"
           >
-            아티스트
+            Artist
           </TextReveal>
 
-          <Line className={"mt-16"} />
+          <Line className={"m-16"} />
+          <div className="flex flex-wrap justify-center gap-16">
+            {artistProfile.getPages().map((artist, index) => (
+              <div key={index}>
+                <div className="h-[300px] w-full object-cover md:h-[520px]">
+                  {artist.name === ARTIST_NAME.LOOZBONE ||
+                  artist.name === ARTIST_NAME.SAM ? (
+                    <CloudinaryImage
+                      src={artist.image}
+                      alt={`Image of ${artist.name}`}
+                      className="h-full w-full object-cover"
+                    />
+                  ) : (
+                    <Image
+                      src={artist.image}
+                      width={1280}
+                      height={600}
+                      alt={`Image of ${artist.name}`}
+                      className="h-full w-full object-cover"
+                      priority
+                    />
+                  )}
+                </div>
+                <div className="l:p-6 flex grow flex-col items-end justify-between gap-4 p-4">
+                  <div className="flex w-full flex-col gap-2">
+                    <div className="l:text-3xl text-2xl leading-8 font-bold tracking-tight text-zinc-950 dark:text-zinc-50">
+                      <TextReveal>{artist.name}</TextReveal>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
     </main>
