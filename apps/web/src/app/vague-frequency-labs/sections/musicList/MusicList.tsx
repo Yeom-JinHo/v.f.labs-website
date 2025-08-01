12604f0eb549d32b7983bf1b76a2c137b5a5a1a2
Yeom-JinHo <pwlsghq@naver.com>
2025-08-01 18:49:03 +0900

    feat: add more button

--- apps/web/src/app/vague-frequency-labs/sections/musicList/MusicList.tsx
+++ apps/web/src/app/vague-frequency-labs/sections/musicList/MusicList.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 import { musicInfo } from "@/app/source";
 import TextReveal from "@/components/fancy/text-reveal";
 import MotionWrap from "@/components/motion-wrap";
 
+import { Button } from "@repo/ui/button";
+import { Icons } from "@repo/ui/icons";
+
 import MusicInfoCard from "./MusicInfoCard";
 
 function MusicList() {
-  const musicInfos = [
+  const dummyMusicInfos = [
     ...musicInfo.getInfos(),
     ...musicInfo.getInfos(),
     ...musicInfo.getInfos(),
@@ -16,6 +20,8 @@ function MusicList() {
     ...musicInfo.getInfos(),
     ...musicInfo.getInfos(),
   ];
+
+  const musicInfos = dummyMusicInfos.slice(0, 9);
 
   return (
     <MotionWrap className="w-full py-24 lg:py-32" id="music-list">
@@ -37,13 +43,11 @@ function MusicList() {
               <MusicInfoCard musicInfo={info} key={info.name + index} />
             ))}
           </div>
-          <Link href="/music" className="mt-12">
-            <TextReveal
-              as="p"
-              className="flex flex-col -space-y-4 text-xl leading-tight font-bold tracking-tighter sm:text-xl md:text-xl md:leading-tight lg:text-xl lg:leading-tight"
-            >
+          <Link href="/vague-frequency-labs/music">
+            <Button className="mt-12">
+              <Icons.add className="mr-2 h-4 w-4" />
               More
-            </TextReveal>
+            </Button>
           </Link>
         </div>
       </div>
