224a89a51c705662fb86bcd4f54ff6e4959f65ed
Yeom-JinHo <pwlsghq@naver.com>
2025-08-01 17:22:32 +0900

    feat: cloudinary image 컴포넌트 사용하던 애들 제거

--- apps/web/src/app/vague-frequency-labs/sections/artistProfiles/ArtistSimpleCard.tsx
+++ apps/web/src/app/vague-frequency-labs/sections/artistProfiles/ArtistSimpleCard.tsx
@@ -1,9 +1,7 @@
 import type { ArtistProfile } from "@/types/artist";
 import React from "react";
 import Image from "next/image";
-import { CloudinaryImage } from "@/components/cloudinary-image";
 import TextReveal from "@/components/fancy/text-reveal";
-import { ARTIST_NAME } from "@/consts/artist";
 
 interface ArtistSimpleCardProps {
   artist: ArtistProfile;
@@ -14,23 +12,14 @@ export default function ArtistSimpleCard({ artist }: ArtistSimpleCardProps) {
     <div>
       <div className="group relative h-[400px] w-[300px] overflow-hidden object-cover md:h-[520px] md:w-[350px]">
         {/* 기본 이미지 */}
-        {artist.name === ARTIST_NAME.LOOZBONE ||
-        artist.name === ARTIST_NAME.SAM ? (
-          <CloudinaryImage
-            src={artist.image}
-            alt={`Image of ${artist.name}`}
-            className="h-full w-full object-cover transition-all duration-300 group-hover:scale-110 group-hover:opacity-30"
-          />
-        ) : (
-          <Image
-            src={artist.image}
-            width={1280}
-            height={600}
-            alt={`Image of ${artist.name}`}
-            className="h-full w-full object-cover transition-all duration-300 group-hover:scale-110 group-hover:opacity-30"
-            priority
-          />
-        )}
+        <Image
+          src={artist.image}
+          width={1280}
+          height={600}
+          alt={`Image of ${artist.name}`}
+          className="h-full w-full object-cover transition-all duration-300 group-hover:scale-110 group-hover:opacity-30"
+          priority
+        />
 
         {/* Hover 시 나타나는 이미지 */}
         <div className="absolute inset-0 flex items-center justify-center opacity-0 transition-all duration-300 group-hover:opacity-100">
