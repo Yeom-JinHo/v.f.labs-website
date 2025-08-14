e7fe8f34e1030435cd278fb6a036eeaa9be68f50
Yeom-JinHo <pwlsghq@naver.com>
2025-08-14 22:59:21 +0900

    feat: celebrate, payday Coming Soon 구성

--- apps/web/src/app/celebrate-agency/page.tsx
+++ apps/web/src/app/celebrate-agency/page.tsx
@@ -1,8 +1,29 @@
+import Link from "next/link";
+import GalaxyBackground from "@/components/common/GalaxyBackground";
+
+import { cn } from "@repo/ui";
+import { buttonVariants } from "@repo/ui/button";
+
 export default function CelebrateAgencyPage() {
   return (
     <div className="flex min-h-[100dvh] flex-col">
-      <main className="flex-1">
-        <h1 className="text-6xl">TBD</h1>
+      <main className="relative h-screen w-screen flex-1">
+        <div className="align-center absolute top-1/2 left-1/2 z-10 flex -translate-x-1/2 -translate-y-1/2 flex-col gap-4">
+          <h1 className="text-6xl">Coming Soon</h1>
+          <Link
+            href="/"
+            className={cn(
+              buttonVariants({ variant: "default" }),
+              "w-full self-center rounded-full border border-white/30 bg-white/20 px-8 py-2 text-white backdrop-blur-sm transition-all duration-300 hover:bg-white/30 md:self-start",
+            )}
+          >
+            Home
+          </Link>
+        </div>
+
+        <div className="absolute inset-0">
+          <GalaxyBackground mouseInteraction={false} />
+        </div>
       </main>
     </div>
   );
--- apps/web/src/app/payday-records/layout.tsx
+++ apps/web/src/app/payday-records/layout.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+import GalaxyBackground from "@/components/common/GalaxyBackground";
+
+import { cn } from "@repo/ui";
+import { buttonVariants } from "@repo/ui/button";
+
 export default function PaydayRecordsLayout({
   children,
 }: Readonly<{
@@ -5,7 +11,24 @@ export default function PaydayRecordsLayout({
 }>) {
   return (
     <div className="flex min-h-[100dvh] flex-col">
-      <main className="flex-1">{children}</main>
+      <main className="relative h-screen w-screen flex-1">
+        <div className="align-center absolute top-1/2 left-1/2 z-10 flex -translate-x-1/2 -translate-y-1/2 flex-col gap-4">
+          <h1 className="text-6xl">Coming Soon</h1>
+          <Link
+            href="/"
+            className={cn(
+              buttonVariants({ variant: "default" }),
+              "w-full self-center rounded-full border border-white/30 bg-white/20 px-8 py-2 text-white backdrop-blur-sm transition-all duration-300 hover:bg-white/30 md:self-start",
+            )}
+          >
+            Home
+          </Link>
+        </div>
+
+        <div className="absolute inset-0">
+          <GalaxyBackground mouseInteraction={false} />
+        </div>
+      </main>
     </div>
   );
 }
