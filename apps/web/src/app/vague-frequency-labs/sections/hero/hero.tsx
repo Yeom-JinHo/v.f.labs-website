521d03854b40822b08325fc5030ffed9671e2b28
Yeom-JinHo <pwlsghq@naver.com>
2025-08-21 00:34:56 +0900

    fix: hero text 위치 조정

--- apps/web/src/app/vague-frequency-labs/sections/hero/hero.tsx
+++ apps/web/src/app/vague-frequency-labs/sections/hero/hero.tsx
@@ -4,13 +4,13 @@ function Hero() {
   return (
     <section className="relative h-[100vh]">
       <div className="top-0 flex h-screen items-center justify-center overflow-x-hidden">
-        <h1 className="absolute top-[20vh] text-4xl font-bold md:top-[10vh]">
+        <h1 className="absolute top-[15vh] text-4xl font-bold md:top-[10vh]">
           We are
         </h1>
         <div className="w-full scale-100 md:scale-90 lg:scale-80">
           <Globe></Globe>
         </div>
-        <h1 className="absolute bottom-[20vh] text-3xl font-bold md:bottom-[10vh]">
+        <h1 className="absolute bottom-[15vh] text-3xl font-bold md:bottom-[10vh]">
           Vague Frequency Labs
         </h1>
       </div>
