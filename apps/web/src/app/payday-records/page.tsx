34e7240f23869d35d74ba33924a034ae5c4502d7
Yeom-JinHo <pwlsghq@naver.com>
2025-09-01 20:09:27 +0900

    feat: payday 1차 레이아웃

--- apps/web/src/app/payday-records/page.tsx
+++ apps/web/src/app/payday-records/page.tsx
@@ -1,8 +1,10 @@
-export default function PaydayRecordsPage() {
+import Hero from "./sections/hero/hero";
+
+export default function Home() {
   return (
     <div className="flex min-h-[100dvh] flex-col">
       <main className="flex-1">
-        <h1 className="text-6xl">TBD</h1>
+        <Hero />
       </main>
     </div>
   );
---
+++ apps/web/src/app/payday-records/sections/header/header.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { linkLimit, links } from "@/app/payday-records/sections/header/config";
+import Link from "@/components/fancy/link";
+
+import { Icons } from "@repo/ui/icons";
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    setMounted(true);
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <header
+      className={[
+        "bg-background/80 fixed z-[900] w-full backdrop-blur-sm",
+        "transform transition-opacity transition-transform duration-[800ms] ease-out will-change-transform",
+        mounted ? "translate-y-0 opacity-100" : "-translate-y-20 opacity-0",
+        "motion-reduce:transform-none motion-reduce:opacity-100 motion-reduce:transition-none",
+      ].join(" ")}
+    >
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <div className="flex w-full justify-between">
+          <Link
+            href="/"
+            className="inline-flex items-center justify-center text-2xl font-semibold"
+          >
+            payday records
+          </Link>
+
+          <button className="md:hidden" onClick={toggleMenu}>
+            <span className="sr-only">{isOpen ? "Close" : "Menu"}</span>
+            {isOpen ? (
+              <Icons.close className="h-6 w-6" />
+            ) : (
+              <Icons.menu className="h-6 w-6" />
+            )}
+          </button>
+          <div className="hidden md:flex md:w-auto md:items-center">
+            <nav className="flex items-center gap-4">
+              <div className="flex items-center gap-4 lg:gap-6">
+                {links.slice(0, linkLimit).map(({ title, href }, index) => (
+                  <Link
+                    className="flex items-center text-xl font-medium underline-offset-4 hover:underline"
+                    href={href}
+                    key={`header-desktop-link_${index}`}
+                  >
+                    {title}
+                  </Link>
+                ))}
+              </div>
+              {/* <div className="flex items-center gap-2">
+                <ThemeToggle />
+              </div> */}
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div
+        className="overflow-hidden bg-transparent md:hidden"
+        style={{
+          maxHeight: isOpen ? "60vh" : 0,
+          transition: "max-height 300ms ease-in-out",
+          pointerEvents: isOpen ? "auto" : "none",
+          visibility: isOpen ? "visible" : "hidden",
+        }}
+      >
+        <div className="flex flex-col gap-4 p-4">
+          {links.map(({ title, href }, index) => (
+            <Link
+              className="flex items-center text-xl font-medium underline-offset-4 hover:underline"
+              href={href}
+              onClick={toggleMenu}
+              key={`header-mobile-link_${index}`}
+            >
+              {title}
+            </Link>
+          ))}
+          {/* <div className="flex w-full items-center justify-end">
+            <ThemeToggle />
+          </div> */}
+        </div>
+      </div>
+    </header>
+  );
+}
