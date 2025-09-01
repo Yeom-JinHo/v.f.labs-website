bf5221985b082df15db566c71133b6a1c6284c13
Yeom-JinHo <pwlsghq@naver.com>
2025-09-01 20:21:28 +0900

    feat: about 페이지

--- apps/web/src/app/payday-records/page.tsx
+++ apps/web/src/app/payday-records/page.tsx
@@ -1,3 +1,4 @@
+import About from "./sections/about/about";
 import Hero from "./sections/hero/hero";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <div className="flex min-h-[100dvh] flex-col">
       <main className="flex-1">
         <Hero />
+        <About />
       </main>
     </div>
   );
