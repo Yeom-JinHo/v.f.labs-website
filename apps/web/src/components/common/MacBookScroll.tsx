0f95118670fd8c3c7c5e8b9f6e5b3f9001fc4d47
Yeom-JinHo <pwlsghq@naver.com>
2025-09-02 19:57:11 +0900

    refactor: nextImage로 교체

--- apps/web/src/components/common/MacBookScroll.tsx
+++ apps/web/src/components/common/MacBookScroll.tsx
@@ -2,6 +2,7 @@
 
 import type { MotionValue } from "motion/react";
 import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
 import {
   IconBrightnessDown,
   IconBrightnessUp,
@@ -167,9 +168,10 @@ export const Lid = ({
         className="absolute inset-0 h-96 w-[32rem] rounded-2xl bg-[#010101] p-2"
       >
         <div className="absolute inset-0 rounded-lg bg-[#272729]" />
-        <img
-          src={src}
+        <Image
+          src={src || ""}
           alt="aceternity logo"
+          fill
           className="absolute inset-0 h-full w-full rounded-lg object-cover object-left-top"
         />
       </motion.div>
