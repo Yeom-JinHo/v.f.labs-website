55dcc19dd343dc0d96b18e179bdb4f6a8918cbba
Yeom-JinHo <pwlsghq@naver.com>
2025-09-02 20:00:50 +0900

    feat: BlurFade> 사용

--- apps/web/src/app/payday-records/sections/hero/hero.tsx
+++ apps/web/src/app/payday-records/sections/hero/hero.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import { BlurFade } from "@/components/common/BlurFade";
 
 function Hero() {
   return (
     <section className="relative flex h-[100vh] items-center justify-center">
-      <Image
-        src="/images/logo/400_300/PAYDAY.png"
-        alt="Payday Records"
-        width={900}
-        height={600}
-      />
+      <BlurFade>
+        <Image
+          src="/images/logo/400_300/PAYDAY.png"
+          alt="Payday Records"
+          width={900}
+          height={600}
+        />
+      </BlurFade>
     </section>
   );
 }
