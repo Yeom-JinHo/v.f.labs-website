9b5e8b0143e3728481e24a94ba46b3dfcf5a8ba0
Yeom-JinHo <81306489+Yeom-JinHo@users.noreply.github.com>
2025-09-05 23:06:43 +0900

    Fix/magic UI iphone (#74)
    * feat: Iphon 15 component
    
    * feat: celeb에 임시 적용 iphone15 ui
    
    * fix: ios overflow clippath
    
    * fix: ios clippath
    
    * fix: ios video masking
    
    * fix: layout
    
    * fix: import error
    
    * fix: zindex
    
    * fix: isolate
    
    * fix: svg fill
    
    * fix: final
    
    * fix: final2
    
    * fix: final4
    
    * feat: 원본 iphone 15 pro component 추가
    
    * Revert "feat: celeb에 임시 적용 iphone15 ui"
    
    This reverts commit 93ae0749d142fd0b64a8c0a5d82af48a8722f7d8.
    
    * feat: 데모사이트
    
    * fix: import error
    
    * feat: src 있을때만 마스킹
    
    * refactor(Iphone15Pro): move sizing responsibility to wrapper with aspect-ratio
    
    * feat: demo
    
    * feat: 최종버전 반영
    
    ---------
    
    Co-authored-by: 진호 <jinho2.yeom@hcs.com>

---
+++ apps/web/src/app/vague-frequency-labs/demo2/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Iphone15Pro } from "@/components/common/Iphone15Pro";
+import TextReveal from "@/components/fancy/text-reveal";
+
+export default function DemoPage() {
+  return (
+    <main className="my-14 flex-1">
+      <section className="relative flex min-h-[calc(50dvh)] items-center justify-center">
+        <div className="flex w-[50vw] w-screen max-w-screen flex-col items-center p-20">
+          {/* todo: re-add delay of 0.2seconds */}
+          <TextReveal
+            as="h1"
+            className="leading-wide tracking-relaxed text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl"
+          >
+            ASIS
+          </TextReveal>
+          <Iphone15Pro
+            className="h-full w-full"
+            videoSrc="https://videos.pexels.com/video-files/8946986/8946986-uhd_1440_2732_25fps.mp4"
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
