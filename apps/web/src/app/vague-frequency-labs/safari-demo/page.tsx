71440ba4fe79735a319a55879bc2c866138b2026
Yeom-JinHo <81306489+Yeom-JinHo@users.noreply.github.com>
2025-09-07 01:56:26 +0900

    Merge pull request #76 from Yeom-JinHo/fix/magicui/safari
    Fix/magicui/safari

---
+++ apps/web/src/app/vague-frequency-labs/safari-demo/page.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Safari } from "@/components/common/safari";
+import { SafariASIS } from "@/components/common/safari-asis";
+import TextReveal from "@/components/fancy/text-reveal";
+
+export default function DemoPage() {
+  return (
+    <main className="my-14 flex-1">
+      <section className="relative flex min-h-[calc(50dvh)] items-center justify-center">
+        <div className="flex w-screen max-w-[50vw] max-w-screen flex-col items-center p-20">
+          {/* todo: re-add delay of 0.2seconds */}
+          <TextReveal
+            as="h1"
+            className="leading-wide tracking-relaxed text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl"
+          >
+            TOBE
+          </TextReveal>
+          <Safari
+            className="h-full w-full"
+            // width={400}
+            videoSrc="https://videos.pexels.com/video-files/27180348/12091515_2560_1440_50fps.mp4"
+          />
+          <TextReveal
+            as="h1"
+            className="leading-wide tracking-relaxed text-5xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl"
+          >
+            ASIS
+          </TextReveal>
+          <SafariASIS
+            className="h-full w-full"
+            videoSrc="https://videos.pexels.com/video-files/27180348/12091515_2560_1440_50fps.mp4"
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
